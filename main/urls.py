14b35cb31264740db71883038c4e20613c0f16cd
rheehyerin <toohappy2122@gmail.com>
2017-02-13 10:40:39 +0900

    이미지 전부 추가

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,22 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # whitening #
     url(r'^whitening/$', views.whitening, name="whitening"),
     url(r'^laser/$', views.laser, name="laser"),
     url(r'^jade/$', views.jade, name="jade"),
     url(r'^targeting/$', views.targeting, name="targeting"),
+
+    # peeling #
+    url(r'^aqua/$', views.aqua, name="aqua"),
+    url(r'^celebrity/$', views.celebrity, name="celebrity"),
+    url(r'^latte/$', views.latte, name="latte"),
+    url(r'^spectra/$', views.spectra, name="spectra"),
+
+    # scar #
+    url(r'^legato/$', views.legato, name="legato"),
+    url(r'^prp/$', views.prp, name="prp"),
+    url(r'^tera/$', views.tera, name="tera"),
+
     url(r'^$', views.main, name="main"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
-
 def main(request):
     return render(request, "main.html")
 
-
+# whitening #
 def whitening(request):
     return render(request, "whitening/whitening.html")
 
@@ -16,4 +15,28 @@ def jade(request):
 
 def targeting(request):
     return render(request, "whitening/targeting.html")
+
+# peeling #
+def aqua(request):
+    return render(request, "peeling/aqua.html")
+
+def celebrity(request):
+    return render(request, "peeling/celebrity.html")
+
+def latte(request):
+    return render(request, "peeling/latte.html")
+
+def spectra(request):
+    return render(request, "peeling/spectra.html")
+
+# scar #
+def legato(request):
+    return render(request, "scar/legato.html")
+
+def prp(request):
+    return render(request, "scar/prp.html")
+
+def tera(request):
+    return render(request, "scar/tera.html")
+
 
