e4d608d4d2f6a6838d280da56926854e9286be3d
rheehyerin <toohappy2122@gmail.com>
2017-02-13 15:40:23 +0900

    사진 다~ 넣었~다~ 룰~루~

--- main/urls.py
+++ main/urls.py
@@ -20,16 +20,33 @@ urlpatterns = [
     url(r'^tera/$', views.tera, name="tera"),
 
     # botox #
-    # url(r'^botox/$', views.botox, name="botox"),
-    # url(r'^filler/$', views.filler, name="filler"),
+    url(r'^botox/$', views.botox, name="botox"),
+    url(r'^filler/$', views.filler, name="filler"),
 
     # petit #
+    url(r'^darkcircle/$', views.darkcircle, name="darkcircle"),
+    url(r'^dermotoxin/$', views.dermotoxin, name="dermotoxin"),
+    url(r'^highnose/$', views.highnose, name="highnose"),
+    url(r'^kotppol/$', views.kotppol, name="kotppol"),
+    url(r'^witch/$', views.witch, name="witch"),
 
     # contour #
+    url(r'^bigeyes/$', views.bigeyes, name="bigeyes"),
+    url(r'^contour/$', views.contour, name="contour"),
+    url(r'^doublero/$', views.doublero, name="doublero"),
+    url(r'^thread/$', views.thread, name="thread"),
 
     # pimple #
+    url(r'^pimple/$', views.pimple, name="pimple"),
 
     # obesity #
+    url(r'^carboxy/$', views.carboxy, name="carboxy"),
+    url(r'^cinderella/$', views.cinderella, name="cinderella"),
+    url(r'^coolshaping/$', views.coolshaping, name="coolshaping"),
+    url(r'^highfrequency/$', views.highfrequency, name="highfrequency"),
+    url(r'^hpl/$', views.hpl, name="hpl"),
+    url(r'^mesotherapie/$', views.mesotherapie, name="mesotherapie"),
+    url(r'^slim/$', views.slim, name="slim"),
 
     url(r'^map/$', views.map, name="map"),
 
--- main/views.py
+++ main/views.py
@@ -39,6 +39,67 @@ def prp(request):
 def tera(request):
     return render(request, "scar/tera.html")
 
+# botox #
+def botox(request):
+    return render(request, "botox/botox.html")
+
+def filler(request):
+    return render(request, "botox/filler.html")
+
+# petit #
+def darkcircle(request):
+    return render(request, "petit/darkcircle.html")
+
+def dermotoxin(request):
+    return render(request, "petit/dermotoxin.html")
+
+def highnose(request):
+    return render(request, "petit/highnose.html")
+
+def kotppol(request):
+    return render(request, "petit/kotppol.html")
+
+def witch(request):
+    return render(request, "petit/witch.html")
+
+# contour #
+def bigeyes(request):
+    return render(request, "contour/bigeyes.html")
+
+def contour(request):
+    return render(request, "contour/contour.html")
+
+def doublero(request):
+    return render(request, "contour/doublero.html")
+
+def thread(request):
+    return render(request, "contour/thread.html")
+
+# pimple #
+def pimple(request):
+    return render(request, "pimple/pimple.html")
+
+# obesity #
+def carboxy(request):
+    return render(request, "obesity/carboxy.html")
+
+def cinderella(request):
+    return render(request, "obesity/cinderella.html")
+
+def coolshaping(request):
+    return render(request, "obesity/coolshaping.html")
+
+def highfrequency(request):
+    return render(request, "obesity/highfrequency.html")
+
+def hpl(request):
+    return render(request, "obesity/hpl.html")
+
+def mesotherapie(request):
+    return render(request, "obesity/mesotherapie.html")
+
+def slim(request):
+    return render(request, "obesity/slim.html")
 
 def map(request):
     return render(request, "map.html")
